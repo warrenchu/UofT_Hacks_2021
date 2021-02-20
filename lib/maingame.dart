93bf3a3e9fad033a7c297a8063d51ba5b08e121f
Warren Chu <76016561+warrenchu@users.noreply.github.com>
2021-02-20 16:27:27 -0500

    before making the bottom navigation bar

--- lib/maingame.dart
+++ lib/maingame.dart
@@ -3,6 +3,8 @@ import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flame/flame.dart';
+import 'package:flutter/material.dart';
+import 'main.dart';
 
 void rungame() async {
 // create util instance to start flame
@@ -13,5 +15,25 @@ void rungame() async {
   Flame.images.loadAll(<String>["assets/images/background.png"]);
 
   GameController gameController = GameController();
-  runApp(gameController.widget);
+  //runApp(gameController.widget);
+  // I decided to run the game within a Stack so that we can render elements
+  // over the background
+  runApp(MaterialApp(
+      title: 'RBC Savings Game',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Welcome!'), actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.arrow_back),
+            tooltip: "Log out",
+            onPressed: () {
+              MyApp();
+            },
+          )
+        ]),
+        body: Stack(
+          fit: StackFit.expand,
+          children: [gameController.widget],
+        ),
+      )));
 }
