a3946b222e843a25adb7c60066f75f3154c1da23
Warren Chu <76016561+warrenchu@users.noreply.github.com>
2021-02-20 10:12:41 -0500

    login screen mostly done

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flame/util.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:rbc_savings_game/game_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:flame_splash_screen/flame_splash_screen.dart';
 
 void main() async {
   // create util instance to start flame
